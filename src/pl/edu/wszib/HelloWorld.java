78407b048bb15163c3ceaae648b8644f06c97705
Bernadeta G <grudzien.bernadeta@example.com>
2020-12-12 09:42:07 +0100

    Dodanie odliczania do 10.

--- src/pl/edu/wszib/HelloWorld.java
+++ src/pl/edu/wszib/HelloWorld.java
@@ -2,6 +2,10 @@ package pl.edu.wszib;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
         System.out.println("Hello World!");
+
     }
 }
